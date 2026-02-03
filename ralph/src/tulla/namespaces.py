5fcfaafda1137cc74bda6d6b703cd10e66cec8aa
tulla (operated by Hans-Peter Zorn) <hpz@zorns.org>
2026-02-03 11:30:47 +0100

    impl(prd:req-67-1-1): Add PHASE_NS to Namespace Registry

--- ralph/src/tulla/namespaces.py
+++ ralph/src/tulla/namespaces.py
@@ -10,12 +10,14 @@ from __future__ import annotations
 PRD_NS = "http://impl-ralph.io/prd#"
 TRACE_NS = "http://impl-ralph.io/trace#"
 ISAQB_NS = "http://impl-ralph.io/isaqb#"
+PHASE_NS = "http://impl-ralph.io/phase#"
 
 # Full URI -> compact prefix (used by p6 hydration)
 PREFIXES: dict[str, str] = {
     PRD_NS: "prd:",
     TRACE_NS: "trace:",
     ISAQB_NS: "isaqb:",
+    PHASE_NS: "phase:",
     "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf:",
     "http://www.w3.org/2000/01/rdf-schema#": "rdfs:",
     "http://www.w3.org/2001/XMLSchema#": "xsd:",
